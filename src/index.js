a6422a27e9163758cd722d3b25a738744c498227
manojvictor <fmanojvictor@gmail.com>
2018-08-21 18:55:41 +0530

    react states with redux

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import BoxCon from './container/box-container'
+import './index.css';
+
+class App extends React.Component {
+    render () {
+        return {
+            <BoxCon></BoxCon>
+        }
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
